60515f4fb47fe4109cf7e1dbfbc0c8c84bcaea50
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-04 22:17:58 -0700

    Update and rename Loader to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 4442272183 then --Blox Fruits Sea 2--
+    
+elseif game.PlaceId == 7449423635 then --Blox Fruits Sea 3--
+    
+elseif game.PlaceId == 2753915549 then --Blox Fruits Sea 1--
