b47ccdaac8ad9e408e9c59a43029e30765c7c7b0
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-04 22:19:17 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,11 @@
 if game.PlaceId == 4442272183 then --Blox Fruits Sea 2--
     _G.AutoConfetti = true
     https://raw.githubusercontent.com/SeasonalKirito/Unpatched/main/BloxFruits%20Confetti.lua
+    
 elseif game.PlaceId == 7449423635 then --Blox Fruits Sea 3--
     _G.AutoConfetti = true
     https://raw.githubusercontent.com/SeasonalKirito/Unpatched/main/BloxFruits%20Confetti.lua
+    
 elseif game.PlaceId == 2753915549 then --Blox Fruits Sea 1--
     _G.AutoConfetti = true
     https://raw.githubusercontent.com/SeasonalKirito/Unpatched/main/BloxFruits%20Confetti.lua
