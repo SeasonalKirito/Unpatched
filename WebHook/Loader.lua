8b5ea861e13028520c7c105a7a5470e4e1d415d6
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2022-12-07 22:19:50 -0700

    Update Loader.lua

--- WebHook/Loader.lua
+++ WebHook/Loader.lua
@@ -1,3 +1,3 @@
-getgenv().DiscordWebhook = "https://discord.com/api/webhooks/1049560621504528395/InwFOKb9SoQPeL9hjaHJTv-rPuDlQaMEDblYelSjdOu4tyfNph1MHFZ7t6dwhs3e1zUF"
+getgenv().DiscordWebhook = ""
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/Unpatched/main/WebHook/POST.lua"))();
